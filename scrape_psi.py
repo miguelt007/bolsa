e03823494d228349b623d2ed90e99df4f2460698
Miguel Teixeira <miguelt007@gmail.com>
2025-09-26 11:51:09 +0100

    Update scrape_psi.py

--- scrape_psi.py
+++ scrape_psi.py
@@ -1,13 +1,16 @@
 import requests
 import json
 
-url = "https://investing-com6.p.rapidapi.com/web-crawling/api/markets/indices"
+url = "https://investing-com6.p.rapidapi.com/web-crawling/api/markets/v2/indices/performance"
 headers = {
-    "X-RapidAPI-Key": "dbcf86698cmshd04b1b46934dd15p11474ejsn267bbdb81678",  # substitui pela tua chave real
+    "X-RapidAPI-Key": "dbcf86698cmshd04b1b46934dd15p11474ejsn267bbdb81678",
     "X-RapidAPI-Host": "investing-com6.p.rapidapi.com"
 }
+params = {
+    "market": "PT"
+}
 
-response = requests.get(url, headers=headers)
+response = requests.get(url, headers=headers, params=params)
 if response.status_code != 200:
     raise Exception(f"Erro na API: {response.status_code}")
 
@@ -15,14 +18,13 @@ dados_brutos = response.json()
 dados_filtrados = []
 
 for item in dados_brutos.get("data", []):
-    if "PSI" in item.get("name", ""):
-        dados_filtrados.append({
-            "indice": item.get("name"),
-            "cotacao": item.get("price"),
-            "variacao": item.get("change"),
-            "percentual": item.get("change_percent"),
-            "hora": item.get("time")
-        })
+    dados_filtrados.append({
+        "indice": item.get("name"),
+        "cotacao": item.get("price"),
+        "variacao": item.get("change"),
+        "percentual": item.get("change_percent"),
+        "hora": item.get("time")
+    })
 
 with open("data/psi.json", "w", encoding="utf-8") as f:
     json.dump(dados_filtrados, f, ensure_ascii=False, indent=2)
